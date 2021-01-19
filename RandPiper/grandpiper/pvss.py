e7b873ce704a87f5d349279be277f1ff944dc2f7
Huangyuan <suhither@gmail.com>
2021-01-19 19:54:43 +0800

    Add primary files

---
+++ RandPiper/grandpiper/pvss.py
@@ -0,0 +1,5 @@
+""" implementation of the publicly-verifiable secret sharing protocol described in
+"""
+
+import hashlib
+
---
+++ RandPiper/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='GRandPiper',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='suhuangyuan',
+    author_email='hsu008@e.ntu.edu.sg',
+    description='GRandPiper protocol'
+)
